0706dc0474aad9cc449e090f85fc8cb0759fdc39
Fei Liu <fliu5@stanford.edu>
2018-03-02 00:48:17 -0800

    io.py is ready

--- src/utils/io.py
+++ src/utils/io.py
@@ -10,7 +10,7 @@ class preprocessing_wrapper(object):
         except OSError:
             if not os.path.isdir(path):
                 raise
-    def prepare_data_folder(self, train_input_path, output_folder_path, train_test_factor, static_config=None, debug_factor=1.0):
+    def prepare_data_folder(self, train_input_path, output_folder_path, train_test_factor, debug_factor=1.0):
         '''
         This method will take the train data and then divide it into train and test sets by a factor of train_test_factor.
         Then the train dataset is splitted according the column name first.
@@ -24,12 +24,10 @@ class preprocessing_wrapper(object):
         :param train_input_path: file path for original training
         :param splitted_output_folder: output folder
         :param train_test_factor: ratio to split train and test
-        :param static_config: config passed in (default under src.config.static_config.py)
         :param debug_factor: if run with this config, only sample a small proportion of the raw data. 1.0 means no debug
         :return: 
         '''
-        if static_config is None:
-            static_config = static_config()
+        self.create_folder(output_folder_path)
         label_cols = ['id', 'comment_text', 'toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
         raw_data = pd.read_csv(train_input_path)
 
@@ -37,17 +35,20 @@ class preprocessing_wrapper(object):
         train = pd.DataFrame(raw_data[:train_data_size],columns = label_cols)
 
         test = pd.DataFrame(raw_data[train_data_size:] if debug_factor == 1.0 else raw_data[-100:],columns = label_cols)
+        test_name = "{}/{}".format(output_folder_path,"test.csv")
+        test.to_csv(test_name, index=False)
 
-        self.create_folder(output_folder_path)
 
         for label_name in ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']:
-            label_output = output_folder_path + label_name
+            label_output = output_folder_path +"/"+ label_name
             self.create_folder(label_output)
             positive_train = train[train[label_name]>0]
+            if positive_train.shape[0] == 0:
+                return
             negative_train = train[train[label_name] == 0]
             ratio = negative_train.shape[0]/positive_train.shape[0]
             slice_size = positive_train.shape[0]
-            for i in xrange(ratio):
+            for i in range(int(ratio)):
 
                 sub_train_df = positive_train
 
@@ -57,9 +58,11 @@ class preprocessing_wrapper(object):
                 sub_train_df = sub_train_df.sample(frac=1).reset_index(drop=True)
                 sub_train_output_file_path = '{}/tr_train_{}.csv'.format(label_output, i)
                 sub_train_df.to_csv(sub_train_output_file_path)#, index=False)
-                print 'output subset {', i, '} to file ', sub_train_output_file_path
-        test.to_csv(output_folder_path+'/test.csv')#, index=False)
+                print ('output subset {} to file '.format(i, sub_train_output_file_path))
 
-def __main__():
+
+
+if __name__ == "__main__":
     wrapper = preprocessing_wrapper()
-    wrapper.prepare_data_folder('../../input/tr_train.csv', '../../preprocessing_wrapper_demo_output')
+    wrapper.prepare_data_folder('./input/train.csv', './preprocessing_wrapper_demo_output',0.7,debug_factor=0.0001)
+
