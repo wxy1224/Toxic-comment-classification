be20775a57edda75cebdb46883e1524d232035e3
FeiLiu <liufei11111@hotmail.com>
2018-03-10 23:19:53 -0800

    label name parameter

--- data_process/augmentation.py
+++ data_process/augmentation.py
@@ -3,7 +3,7 @@ import pandas as pd, numpy as np
 import random
 
 
-def augmentation(orig_tain_path, names_list_path, output_path):
+def augmentation(orig_tain_path, names_list_path, output_path,label_name = "identity_hate"):
 	label_cols = ['id','comment_text','toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 
 	train = pd.read_csv(orig_train_path)
@@ -14,7 +14,7 @@ def augmentation(orig_tain_path, names_list_path, output_path):
 	names = names.split("\n")
 	n = len(names)
 	print(names[0])
-	selected = train.loc[train["identity_hate"]==1].as_matrix()
+	selected = train.loc[train[label_name]==1].as_matrix()
 	# select_comments = selected["comment_text"]
 	# comments = select_comments.as_matrix()
 	construct = []
@@ -33,7 +33,7 @@ def augmentation(orig_tain_path, names_list_path, output_path):
 					modified = comment.replace(s, new_word)
 					# print(s, new_word, modified)
 					new_selected_row = selected_row.copy()
-					new_selected_row[1] = modified
+					new_selected_row[label_cols.index(label_name)] = modified
 					construct.append(new_selected_row)
 				# break
 		# if i%100==0 and len(construct)>0:
